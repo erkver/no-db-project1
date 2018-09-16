9c54fbde39fce667bde4a2490a61d111a0e314f0
Eric Vere <erkver250@gmail.com>
2018-09-15 22:31:36 -0500

    Added post and delete routes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,5 @@
 import React,{ Component } from "react";
-import './App.css';
+import '../App.css';
 import Button from "./Button";
 
 class Input extends Component {
@@ -9,23 +9,29 @@ class Input extends Component {
             userInput: ""
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e) {
-        console.log(this.state.userInput);
+        // console.log(this.state.userInput);
         this.setState({userInput: e.target.value});
     }
 
+    handleClick() {
+        this.props.search(this.state.userInput);
+    }
 
     render() {
         return (
             <div className="input">
                 <input 
-                placeholder="Enter new brewery" 
-                value={this.state.userInput} 
-                onChange={this.handleChange}
+                    placeholder="Find a brewery" 
+                    value={this.state.userInput} 
+                    onChange={this.handleChange}
                 />
-                <Button />
+                <Button 
+                    handleSubmit={() => this.handleClick}
+                />
             </div>
         );
     }
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React from "react";
-import beerPic from './beer-stock-pic.jpg';
-import './App.css';
+import beerPic from '../beer-stock-pic.jpg';
+import '../App.css';
 import { FaBeer } from "react-icons/fa";
 
 const List = props => {
@@ -10,6 +10,12 @@ const List = props => {
             <div className="list">{props.brewery.name} - {props.brewery.brewery_type}</div>
             <div className="list">{props.brewery.city}, {props.brewery.state}</div>
             <div className="list">{props.brewery.website_url}</div>
+            <button onClick={() => 
+                {props.handleToggle();
+                props.toggleForm(props.brewery.id); 
+                props.editBrewery(props.brewery.id)
+                }}>Edit brewery</button>
+            <button onClick={() => props.deleteBrewery(props.brewery.id)}>Delete brewery</button>
         </div>
     );
 }
